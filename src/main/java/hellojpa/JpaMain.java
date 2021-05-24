cdb290f106a545ac70ee327e5152237720399e5d
Conquerex <conquerex@gmail.com>
2021-05-24 16:29:35 +0900

    영속성 컨텍스트 1 ~ 영속성 컨텍스트 2

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -14,24 +14,57 @@ public class JpaMain {
         tx.begin();
 
         try {
+//            // 비영속
 //            Member member = new Member();
-//            member.setId(2L);
-//            member.setName("Kee");
+//            member.setId(101L);
+//            member.setName("Sample");
+//
+//            // 영속
+//            System.out.println(">>>> before");
 //            em.persist(member);
+//            System.out.println(">>>> after");
 
-            Member findMember = em.find(Member.class, 1L);
-//            System.out.println("findMember.getId() = " + findMember.getId());
-//            System.out.println("findMember.getName() = " + findMember.getName());
-            findMember.setName("Hello");
+            // 별도의 Select 쿼리 없이 조회
+            Member findMember = em.find(Member.class, 101L);
+            System.out.println("findMember.getId() = " + findMember.getId());
+            System.out.println("findMember.getName() = " + findMember.getName());
 
-            List<Member> result = em.createQuery("select m from Member m", Member.class)
-                    .setFirstResult(1)
-                    .setMaxResults(100)
-                    .getResultList();
+            System.out.println(">>>>>>> findMember2 ; select 쿼리 있음 >>>>>>>");
+            Member findMember2 = em.find(Member.class, 2L);
+            System.out.println("findMember.getId() = " + findMember2.getId());
+            System.out.println("findMember.getName() = " + findMember2.getName());
 
-            for (Member member : result) {
-                System.out.println("member >>>>> " + member.getName());
-            }
+            System.out.println(">>>>>>> findMember3 ; select 쿼리 X >>>>>>>");
+            Member findMember3 = em.find(Member.class, 2L);
+            System.out.println("findMember.getId() = " + findMember3.getId());
+            System.out.println("findMember.getName() = " + findMember3.getName());
+
+            System.out.println(">>>>> findMember2 == findMember3 : ");
+            System.out.println(">>>>> " + (findMember2 == findMember3));
+
+//            List<Member> result = em.createQuery("select m from Member m", Member.class)
+//                    .setFirstResult(1)
+//                    .setMaxResults(100)
+//                    .getResultList();
+//
+//            for (Member m : result) {
+//                System.out.println("member >>>>> " + m.getName());
+//            }
+
+            /**
+             * 엔티티 등록 예시
+             */
+//            Member member1 = new Member(201L, "aaa");
+//            Member member2 = new Member(202L, "bbbb");
+//            em.persist(member1);
+//            em.persist(member2);
+//            System.out.println("========= After em.persist =======");
+
+            /**
+             * 엔티티 수정 예시
+             */
+            Member sample = em.find(Member.class, 101L);
+            sample.setName("qqqq");
 
             tx.commit();
         } catch (Exception e) {
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -6,6 +6,16 @@ import javax.persistence.Id;
 @Entity
 public class Member {
 
+    public Member() {
+        // JPA는 기본 생성자가 필요
+        // 리플렉션등의 이유로 동적으로 처리되어야 해서
+    }
+
+    public Member(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     @Id
     private Long id;
     private String name;
