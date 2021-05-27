34340ad3e9dec0594c4a726f88d4259a363c53ea
Conquerex <conquerex@gmail.com>
2021-05-27 15:21:51 +0900

    값 타입과 불변 객체 ~ 값 타입의 비교

--- src/main/java/hellojpa/Chapter09Main.java
+++ src/main/java/hellojpa/Chapter09Main.java
@@ -13,11 +13,34 @@ public class Chapter09Main {
         tx.begin();
 
         try {
+            NewAddress address = new NewAddress("ccc", "ddd", "eeee");
+
             NewMember member = new NewMember();
             member.setUsername("nnnname");
-            member.setHomeAddress(new NewAddress("ccc", "ddd", "eeee"));
-
+            member.setHomeAddress(address);
             em.persist(member);
+
+            // 불변객체 변경하기
+            NewAddress newAddress = new NewAddress("Ciiiity", address.getStreet(), address.getZipcode());
+            member.setHomeAddress(newAddress);
+
+
+            NewAddress address2 = new NewAddress(address.getCity(), address.getStreet(), address.getZipcode());
+
+            NewMember member2 = new NewMember();
+            member2.setUsername("mymy");
+            member2.setHomeAddress(address2);
+            em.persist(member2);
+
+//            member.getHomeAddress().setCity("ccccity");
+
+            /**
+             * 값 타입의 비교
+             */
+            NewAddress a1 = new NewAddress("ccc", "ddd", "eeee");
+            NewAddress a2 = new NewAddress("ccc", "ddd", "eeee");
+            System.out.println(">>>>> a1 == a2    ::: " + (a1 == a2));
+            System.out.println(">>>>> a1 equal a2 ::: " + a1.equals(a2));
 
             tx.commit();
         } catch (Exception e) {
--- src/main/java/hellojpa/NewAddress.java
+++ src/main/java/hellojpa/NewAddress.java
@@ -1,6 +1,7 @@
 package hellojpa;
 
 import javax.persistence.Embeddable;
+import java.util.Objects;
 
 @Embeddable
 public class NewAddress {
@@ -42,5 +43,18 @@ public class NewAddress {
 
     public void setZipcode(String zipcode) {
         this.zipcode = zipcode;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        NewAddress that = (NewAddress) o;
+        return Objects.equals(city, that.city) && Objects.equals(street, that.street) && Objects.equals(zipcode, that.zipcode);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(city, street, zipcode);
     }
 }
